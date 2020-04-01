c7c7b14016c88c0fa32468c78da66acc19170820
Lucas C. <61256306+serkeira@users.noreply.github.com>
2020-04-01 02:02:38 -0300

    Novos exercícios

---
+++ CursoLogicaCapitulo08/src/Exercicio04.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+		
+		Exercicio04 app = new Exercicio04();
+		app.start();
+	}
+	
+	private void start() {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Tabuada de: ");
+		Integer multiplicando = scanner.nextInt();
+		while (multiplicando == 0)
+		{
+			System.out.print("Favor digitar um número diferente de 0: ");
+			multiplicando = scanner.nextInt();
+		}
+		
+		imprimirTabuada(multiplicando, 0);
+		
+		scanner.close();
+	
+	}
+	
+	static void imprimirTabuada(Integer multiplicando, Integer multiplicador) {
+		System.out.println(multiplicando + "x" + multiplicador + " = " + (multiplicando * multiplicador));
+		
+		if (++multiplicador <= 10) {
+			imprimirTabuada(multiplicando, multiplicador); 
+		}
+	}
+
+}
+
